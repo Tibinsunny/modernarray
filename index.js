464f30bb783225c7c133ce98ce53590f732384fa
tibinsunny <tibinsunny95@gmail.com>
2021-07-23 21:17:29 +0530

    delete all instead just one

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ exports.popByIndex = function(array,index) {
 array.splice(index, 1)
 return array
   }
-exports.popByValue = function (array, value) {
+exports.popOneByValue = function (array, value) {
+    let newArray = []
+
     if(typeof(array[0])=="number"){
         let index = array.indexOf(value)
         if(index>-1)
@@ -17,18 +19,20 @@ exports.popByValue = function (array, value) {
     }
     if(typeof(array[0])=="object" && typeof(value)=="object")
     {
-        
-        array.map((data,index2) =>{
-            Object.keys(data).map((internal,i) => {
-                if(data[internal]==value[internal]){
-                    array.splice(index2, 1)
-                    return array
-                }
-
+        array.map((data,index) =>{
+            Object.keys(value).map((internal,i) => {
+              if(data[internal]==value[internal]){
+             
+              }
+              else
+              {
+                newArray.push(data)
+              }
             })
         })
+        return newArray
     }
-    return array
+
 }
 exports.pushToIndex = function(array,index,value) {
     array.splice(index, 0, value);
